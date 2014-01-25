612610ed92a5cf5fb1eff538f9b69323fde81abb
Brittany Welsh <brittany.welsh@gmail.com>
2014-01-25 13:42:00 -0800

    Added player interaction via keyboard.

--- geometrydemo.js
+++ geometrydemo.js
@@ -1,5 +1,11 @@
 var gameFPS = 30;
 
+// dealing with keyboard events; plus handy keycode shortcuts
+window.addEventListener("keydown", dealWithKeyboard, false);
+var keyCodeLeftArrow = "37";
+var keyCodeUpArrow = "38";
+var keyCodeRightArrow = "39";
+
 //create stage
 var canvas = document.getElementById("geoworld-canvas");
 var stage = new createjs.Stage(canvas);
@@ -36,9 +42,22 @@ stage.addChild(testLine);
 testLine.yIntercept = 0;    //player space
 testLine.slope = 1;			//player space
 
+// Create player character
+var playerCharacter = new Player();
+playerCharacter.initialize();
+stage.addChild(playerCharacter);
+
+// keyboard events
+function dealWithKeyboard(event) {
+    if (event.keyCode == keyCodeLeftArrow) { playerCharacter.moveLeft(); }
+    if (event.keyCode == keyCodeRightArrow) { playerCharacter.moveRight(); }
+    //if (event.keyCode == keyCodeUpArrow) { playerCharacter.jump(); }
+}
+
 // Game loop
 createjs.Ticker.setFPS(gameFPS);
 createjs.Ticker.addEventListener("tick", function (tick) {
     testLine.render();
+    playerCharacter.render();
     stage.update();
 });
--- player.js
+++ player.js
@@ -1,1 +1,35 @@
 
+function Player() {
+}
+
+Player.prototype = new createjs.Shape();
+
+Player.prototype.initialize = function () {
+
+	this.x = 100;
+	this.y = 100;
+	this.radius = 15;
+	this.speed = 10;
+
+};
+
+Player.prototype.render = function () {
+
+	// draw the player
+    this.graphics.clear();
+    this.graphics.beginFill("red").drawCircle( this.x, this.y, this.radius );
+
+};
+
+Player.prototype.moveLeft = function() {
+	this.x -= this.speed;
+}
+
+Player.prototype.moveRight = function() {
+	this.x += this.speed;
+}
+
+Player.prototype.tick = function (event) {
+
+};
+
