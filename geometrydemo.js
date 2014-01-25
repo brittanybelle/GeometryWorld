52b05fe9133bc100854514f7edc10bf97a41e548
Brittany Welsh <brittany.welsh@gmail.com>
2014-01-25 11:49:42 -0800

    First commit: we can draw a grid and draw arbitrary lines.

---
+++ geometrydemo.js
@@ -0,0 +1,71 @@
+
+//create stage
+var canvas = document.getElementById("geoworld-canvas");
+var stage = new createjs.Stage(canvas);
+
+// determine grid parameters
+var gridWidth = 50;
+var gridHeight = gridWidth;
+var cellSize = canvas.width / gridWidth;
+var gridLeft = 0;
+var gridRight = canvas.width;
+var gridTop = 0;
+var gridBottom = canvas.height;
+
+// create grid object
+var gridLines = new createjs.Shape();
+stage.addChild(gridLines);
+
+// draw the grid
+gridLines.graphics.beginStroke("lightblue");
+
+for(var i = 1; i < gridWidth; ++i) { // Rows
+    gridLines.graphics.moveTo(gridLeft, gridTop + i * cellSize);
+    gridLines.graphics.lineTo(gridRight, gridTop + i * cellSize);
+}
+
+for(var i = 1; i < gridHeight; ++i) { //Cols
+    gridLines.graphics.moveTo(gridLeft + i * cellSize, gridTop);
+    gridLines.graphics.lineTo(gridLeft + i * cellSize, gridBottom);
+}
+
+
+
+
+
+
+
+// Test line
+var testLine = new createjs.Shape();
+stage.addChild(testLine);
+testLine.yIntercept = 0;    //player space
+testLine.slope = 1;			//player space
+
+
+function renderLine() {
+    testLine.graphics.clear();
+
+    // The following var's are in "player space" (referenced to visible grid, not canvas):
+    var xFrom = 0;
+    var xTo = gridWidth;
+
+    var yFrom = testLine.yIntercept;
+    var yTo = testLine.slope*xTo + testLine.yIntercept;
+
+    // Set graphics
+	testLine.graphics.beginStroke("black");
+	testLine.graphics.moveTo( xFrom*cellSize, canvas.height - yFrom*cellSize );
+	testLine.graphics.lineTo( xTo*cellSize, canvas.height - yTo*cellSize );
+
+/*	console.log("yFrom = " + yFrom);
+	console.log("yTo = " + yTo);
+	console.log("xFrom = " + xFrom);
+	console.log("xTo = " + xTo);
+*/
+}
+
+testLine.yIntercept = 30;
+testLine.slope = -2;
+
+renderLine();
+stage.update(); 
