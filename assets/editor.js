e57d492bc0214e5af26038fa352834aea06d056f
William Ono <wmono+git@soundwave.net>
2014-01-26 14:47:10 -0800

    Quick and dirty fix to move edit boxx off the robot

--- assets/editor.js
+++ assets/editor.js
@@ -28,22 +28,27 @@ function Editor(initialYIntercept, initialSlope) {
     };
 
     self.move = function (y, slope) {
+	    var x = 0;
         var canvasOffset = $("#geoworld-canvas").offset();
-        var formBoxHeight = formBox.height();
+        var formBoxHeight = formBox.height() + 50;
 
-        if (slope < 0) {
+	    if (playerCharacter && playerCharacter.x < 150) {
+			x = 200;
+	    }
+
+	    if (slope < 0) {
             // Position box above: Move top up by height of form box
             y = y - formBoxHeight;
         }
 
         // Clamp formBox to screen
-        if (y < 0) {
-            y = 0;
+        if (y < 70) {
+            y = 70;
         } else if (y > canvas.height - formBoxHeight) {
             y = canvas.height - formBoxHeight;
         }
 
-        formBox.css({ position: "absolute", left: canvasOffset.left, top: canvasOffset.top + y });
+        formBox.css({ position: "absolute", left: canvasOffset.left + x, top: canvasOffset.top + y });
     };
 
     self.reset = function () {
