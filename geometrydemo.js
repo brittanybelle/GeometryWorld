42d7d4a2dbd0603abd5a5db652d8a23ba5412600
Brittany Welsh <brittany.welsh@gmail.com>
2014-01-25 19:38:30 -0800

    Physics now works very smoothly (jumping/movement).

--- geometrydemo.js
+++ geometrydemo.js
@@ -1,10 +1,8 @@
 var gameFPS = 30;
 
-// dealing with keyboard events; plus handy keycode shortcuts
-window.addEventListener("keydown", dealWithKeyboard, false);
-var keyCodeLeftArrow = "37";
-var keyCodeUpArrow = "38";
-var keyCodeRightArrow = "39";
+// Listen for 'keydown' and 'keyup' events
+window.addEventListener("keydown", dealWithKeyDown, false);
+window.addEventListener("keyup", dealWithKeyUp, false);
 
 //create stage
 var canvas = document.getElementById("geoworld-canvas");
@@ -46,17 +44,11 @@ var playerCharacter = new Player();
 playerCharacter.initialize();
 stage.addChild(playerCharacter);
 
-// keyboard events
-function dealWithKeyboard(event) {
-    if (event.keyCode == keyCodeLeftArrow) { playerCharacter.moveLeft(); }
-    if (event.keyCode == keyCodeRightArrow) { playerCharacter.moveRight(); }
-    if (event.keyCode == keyCodeUpArrow) { playerCharacter.jump(); }
-}
-
 // Game loop
 createjs.Ticker.setFPS(gameFPS);
 createjs.Ticker.addEventListener("tick", function (tick) {
     testLine.render();
+    passKeyInfoToPlayerController(playerCharacter);
     playerCharacter.resolvePhysics(testLine);
     playerCharacter.render();
     stage.update();
