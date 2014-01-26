0b8984fba17873d9c17ee6a266a2487994e8ea5b
William Ono <wmono+git@soundwave.net>
2014-01-26 09:49:45 -0800

    Play placeholder sound when player wins a level

--- assets/levelgenerator.js
+++ assets/levelgenerator.js
@@ -1,3 +1,6 @@
+var winSound = new Audio("assets/sound/win.wav");
+winSound = 0.8;
+
 function LevelGoal() { }
 LevelGoal.prototype = new createjs.Shape();
 LevelGoal.prototype.initialize = function (stageParent, xPos, yPos, rad) {
@@ -45,6 +48,7 @@ var checkWinConditions = function(playerObject, goalPositionX, goalPositionY) {
 	if (!playerHasWon) {
 		if ( leftBoundPlayer < goalPositionX && rightBoundPlayer > goalPositionX ){
 			if ( upperBoundPlayer < goalPositionY && lowerBoundPlayer > goalPositionY ) {
+                winSound.play();
 				alert("YOU WINNNNNN");
 				playerHasWon = true;
 			}
